ef52e3eee03cb73e6e10ed59aa3fa4b49646d257
Sergei Giniatulin <kornburn@gmail.com>
2024-07-21 18:49:46 +0300

    Create output branch
    Added plot file and remove everything else

--- python-requests/python-requests.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "http://127.0.0.1:8000/get"
-s = requests.Session()
-
-for i in range(1000):
-    response = s.get(url, headers={"connection": "keep-alive"})
--- python-urllib3/python-urllib3.py
+++
@@ -1,7 +0,0 @@
-import urllib3
-
-url = "http://127.0.0.1:8000/get"
-http = urllib3.PoolManager()
-
-for i in range(1000):
-    response = http.request("GET", url, headers={"connection": "keep-alive"})
