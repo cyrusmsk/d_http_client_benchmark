59949be4cefdc6111dfaa77110bf9016de9d05e7
Sergei Giniatulin <kornburn@gmail.com>
2024-09-12 23:05:05 +0300

    Added D server
    based on serverino

---
+++ dlang-server/source/app.d
@@ -0,0 +1,24 @@
+import serverino;
+import std.datetime: Duration, seconds;
+import std.array: split;
+import std.algorithm: startsWith;
+
+mixin ServerinoMain;
+
+@onServerInit ServerinoConfig configure()
+{
+    return ServerinoConfig
+        .create()
+        .setHttpTimeout(15.seconds)
+        .enableKeepAlive(180.seconds)
+        .addListener("0.0.0.0", 8000)
+        .setWorkers(25);
+}
+
+@endpoint void hello(Request req, Output output) {
+    if (req.uri == "/get" && req.method == Request.Method.Get)
+        output ~= "Dlang ser";
+    else
+        if (req.uri.startsWith("/user/") && req.method == Request.Method.Get)
+            output ~= req.uri.split("/user/")[1];
+}
