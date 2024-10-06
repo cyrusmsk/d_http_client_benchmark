403a917c01e6bfc70fd2437fde406117394d4779
Sergei Giniatulin <kornburn@gmail.com>
2024-10-06 22:32:10 +0300

    Update bench.sh
    fix bug with commands

--- bench.sh
+++ bench.sh
@@ -5,8 +5,8 @@ required_bins=('cargo' 'go' 'python' 'dub' 'hyperfine')
 rust_bins=('rust-reqwest' 'rust-ureq')
 go_bins=('go-http-client')
 dotnet_bins=('dotnet-http-client')
-python_bins=('python-requests' 'python-httpx')
-dlang_bins=('dlang-server' 'dlang-arsd' 'dlang-requests')
+python_bins=('python-requests' 'python-urllib' 'python-httpx')
+dlang_bins=('dlang-server' 'dlang-arsd' 'dlang-vibed' 'dlang-requests')
 
 for required_bin in "${required_bins[@]}"; do
   if ! command -v "${required_bin}" &>/dev/null; then
@@ -53,10 +53,12 @@ args=(
   "--command-name" "go-http-client"
   "--command-name" "dotnet-http-client"
   "--command-name" "python-requests"
+  "--command-name" "python-urllib"
   "--command-name" "python-httpx"
   "--command-name" "rust-reqwest"
   "--command-name" "rust-ureq"
   "--command-name" "dlang-arsd"
+  "--command-name" "dlang-vibed"
   "--command-name" "dlang-requests"
 )
 
@@ -67,7 +69,7 @@ for go_bin in "${go_bins[@]}"; do
 done
 
 for dotnet_bin in "${dotnet_bins[@]}"; do
-  commands=("${cpwd}/bin/Release/net8.0/${dotnet_bin}")
+  commands+=("${cpwd}/bin/Release/net8.0/${dotnet_bin}")
 done
 
 for python_bin in "${python_bins[@]}"; do
