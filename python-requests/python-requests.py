1278a5e6b518bbba19331d94831926bb3840407a
Sergei Giniatulin <kornburn@gmail.com>
2024-10-06 20:46:01 +0300

    Update python-requests.py
    Return session to the python requests

--- python-requests/python-requests.py
+++ python-requests/python-requests.py
@@ -1,9 +1,10 @@
 import requests
 
 url = "http://127.0.0.1:8000/get"
+s = requests.Session()
 
 result = 0
 for i in range(5_000):
-    response = requests.get(url, headers={"connection": "keep-alive"})
+    response = s.get(url, headers={"connection": "keep-alive"})
     result = result + int(response.content)
 print(result)
