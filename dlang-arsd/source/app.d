c39692f9bd5983565c60adfd1a854306dc06f5b9
Sergei Giniatulin <kornburn@gmail.com>
2025-08-03 02:28:10 +0300

    [arsd.http2] Async code
    non-blocking requests used

--- dlang-arsd/source/app.d
+++ dlang-arsd/source/app.d
@@ -7,10 +7,17 @@ void main()
 	auto client = new HttpClient();
 	client.keepAlive = true;
 	int result;
-	foreach (_; 0 .. 5_000)
+    HttpRequest[5_000] requests;
+    HttpResponse response;
+
+	foreach (i; 0 .. 5_000)
 	{
-		auto request = client.request(Uri("http://127.0.0.1:8000/get"));
-		auto response = request.waitForCompletion();
+		requests[i] = client.request(Uri("http://127.0.0.1:8000/get"));
+        requests[i].send();
+    }
+    foreach (i; 0 .. 5_000)
+    {
+		response = requests[i].waitForCompletion();
 		result += response.contentText.to!int;
 	}
 	writeln(result);
